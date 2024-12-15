705dcd7beb02332e19dfe92e5952961dbbdf3ea1
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-12-15 13:09:36 -0300

    Update source.sh

--- source.sh
+++ source.sh
@@ -1,1 +1,12 @@
-.....
+pacman -S kodi
+pacman -S 
+pacman -S openbox
+pacman -S xfce-panel thunar mousepad xterm
+pacman -S jq kitty
+pacman -S xorg-xrandr arandr feh
+pacman -S flatpak
+flatpak install com.google.Chrome
+flatpak install com.obsproject.Studio
+flatpak install com.stremio.Stremio
+
+
