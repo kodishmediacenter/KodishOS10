831d1454c118264fd49980e2d2c7f705d80acee5
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-12-15 13:13:55 -0300

    Update source.sh

--- source.sh
+++ source.sh
@@ -1,5 +1,9 @@
 pacman -S kodi
-pacman -S 
+pacman -S wget 
+pacman -S git
+pacman -S firefox
+pacman -S lightdm lightdm-gtk-greeter
+systemctl enable lightdm.service
 pacman -S openbox
 pacman -S xfce-panel thunar mousepad xterm
 pacman -S jq kitty
